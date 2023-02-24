0c183ad60be2150ff4f804e352ce8cb6bd653c8a
goatfanboi23 <python.goat@gmail.com>
2023-02-23 20:51:20 -0500

    forgot to add these files on past commit

--- amclLocal/send_map.m
+++ amclLocal/send_map.m
@@ -1,16 +1,25 @@
 % Clear matlab memory
 clear
+
 % if ros is intitalized in matlab stop it
 rosshutdown
-% IP adress of ros server
-ipaddress = 'http://10.11.6.38:11311';
+% IP adre
+% ss of ros server
+ipaddress = 'http://10.40.48.95:11311';
 % connect to ros server
 rosinit(ipaddress);
 % setup publisher for map
-[mapPublisher,msg] = rospublisher('/my_map',"nav_msgs/OccupancyGrid","DataFormat","struct");
+[mapPublisher,msg] = rospublisher('/map',"nav_msgs/OccupancyGrid","DataFormat","struct");
 % load the map 
-load officemap.mat
+
+%load officemap.mat
+% image = imread('testMap.pgm');
+% imageNorm = double(image)/255;
+% imageOccupancy = 1 - imageNorm;
+% map = binaryOccupancyMap(imageOccupancy,30*56);
 % populate ros message with map data
+map = binaryOccupancyMap(2048,2048);
 msg = rosWriteBinaryOccupancyGrid(msg,map);
 % send the data to ros
 send(mapPublisher,msg);
+fprintf("SentMap\n");
